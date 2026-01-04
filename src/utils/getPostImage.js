850986f063218bb7d4848b012f1406ea5f270967
Minh <truonghaanhminh@gmail.com>
2026-01-05 00:53:33 +0700

    e

--- src/utils/getPostImage.js
+++ src/utils/getPostImage.js
@@ -1,13 +1,13 @@
-import img101 from '../assets/posts/101.jpg';
-import img102 from '../assets/posts/102.jpg';
-import img103 from '../assets/posts/103.jpg';
-import img104 from '../assets/posts/104.jpg';
-import img105 from '../assets/posts/105.jpg';
-import img106 from '../assets/posts/106.jpg';
-import img107 from '../assets/posts/107.jpg';
-import img108 from '../assets/posts/108.jpg';
-import img109 from '../assets/posts/109.jpg';
-import img110 from '../assets/posts/110.jpg';
+import img101 from '../assets/posts/201.jpg';
+import img102 from '../assets/posts/202.jpg';
+import img103 from '../assets/posts/203.jpg';
+import img104 from '../assets/posts/204.jpg';
+import img105 from '../assets/posts/205.jpg';
+import img106 from '../assets/posts/206.jpg';
+import img107 from '../assets/posts/207.jpg';
+import img108 from '../assets/posts/208.jpg';
+import img109 from '../assets/posts/209.jpg';
+import img110 from '../assets/posts/210.jpg';
 
 const mockImages = {
     101: img101,
